5537a1f0b43f5b08b08ed6352a07af522b1c5ec6
Miguel <miguel.fabian.bernal@gmail.com>
2019-07-05 00:17:27 -0500

    inicio validacion compuesta

--- src/components/Publications/index.js
+++ src/components/Publications/index.js
@@ -3,9 +3,12 @@ import { connect } from "react-redux";
 import { usuariosTraerDatos } from "../../actions/usuariosActions";
 import { publicacionesTraerPorUsuario } from "../../actions/publicacionesActions";
 import Spinner from "../../components/general/Spinner";
+import Fatal from "../../components/general/Fatal";
 
 const Publications = ({
-  match,
+  match: {
+    params: { key }
+  },
   users,
   usuariosTraerDatos,
   publicacionesTraerPorUsuario,
@@ -14,69 +17,35 @@ const Publications = ({
 }) => {
   useEffect(() => {
     const verficarUsuarios = async () => {
-      console.log("aparece antes que el primero");
-      console.log(usuariosReducer);
-      console.log(usuariosReducer.users);
-
       if (!usuariosReducer.users.length) {
         await usuariosTraerDatos();
-        console.log("aparece primero");
-        // console.log("declarado primero");
-        // console.log(usuariosReducer);
-        // console.log(usuariosReducer.users);
-        console.log(match.params.key);
-        console.log(usuariosReducer);
-        console.log(usuariosReducer.users);
-        // console.log(users);
       }
-      console.log("aparece segundo");
-      console.log(match.params.key);
-      console.log(usuariosReducer);
-      console.log(usuariosReducer.users);
-
-      // !("publicationsKey" in usuariosReducer.users[match.params.key]) &&
-      //   publicacionesTraerPorUsuario(match.params.key);
     };
     verficarUsuarios();
-    console.log("aparece tercero");
-    // console.log("declarado tercero");
   }, []);
 
   useEffect(() => {
-    console.log("aparece en useEffect");
-    console.log(usuariosReducer);
-    console.log(usuariosReducer.users);
-    console.log(usuariosReducer.users[match.params.key]);
-
     if (usuariosReducer.users.length) {
-      console.log(
-        !("publicationsKey" in usuariosReducer.users[match.params.key])
-      );
+      if (!("publicationsKey" in usuariosReducer.users[key])) {
+        publicacionesTraerPorUsuario(key);
+      }
     }
-    // !("publicationsKey" in usuariosReducer.users[match.params.key]) &&
-    //   publicacionesTraerPorUsuario(match.params.key);
-    //  }, [usuariosReducer]);
-  }, [usuariosReducer.users]);
-
-  // console.log(usuariosReducer);
-  // console.log(publicacionesReducer);
-
-  // console.log("aparece al final");
-  // console.log(usuariosReducer);
-  // console.log(usuariosReducer.users);
+  }, [usuariosReducer.users, key, usuariosReducer.error]);
 
   const locateUser = () => {
-    if (usuariosReducer.loading) {
+    if (usuariosReducer.loading || !usuariosReducer.users.length) {
       return <Spinner />;
     }
+    if (usuariosReducer.error) {
+      return <Fatal message={usuariosReducer.error} />;
+      // console.log("Hay error");
+    }
+    const name = usuariosReducer.users[key].name;
+    return <h1>Publicaciones de {name}</h1>;
   };
-
-  return (
-    <div>
-      <h1>Publicaciones de </h1>
-      {locateUser()}
-    </div>
-  );
+  console.log(usuariosReducer.users);
+  console.log(publicacionesReducer.publications);
+  return <div>{locateUser()}</div>;
 };
 
 const mapStateToProps = ({ usuariosReducer, publicacionesReducer }) => ({
